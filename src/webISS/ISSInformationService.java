b1786d1127d62cbcef086f9e8f9ab2f8c0c52867
Rayyan Memon <micropachycephalosaurs@gmail.com>
2019-11-19 12:28:33 -0600

    new file:   assign2.txt 	new file:   build.gradle.kts 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   src/webISS/GeographicTime.java 	new file:   src/webISS/ISSInformationService.java 	new file:   src/webISS/ISSWebService.java 	new file:   src/webISS/Main.java 	new file:   src/webISS/OpenNotifyWebService.java 	new file:   test/webISS/GeographicTimeTests.java 	new file:   test/webISS/ISSWebServiceTests.java 	new file:   test/webISS/OpenNotifyWebServiceTest.java 	new file:   tests.txt

---
+++ src/webISS/ISSInformationService.java
@@ -0,0 +1,29 @@
+package webISS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ISSInformationService implements ISSWebService{
+  public long fetchIssFlyOverData(double latitude, double longitude) throws IOException {
+    String requestURL = "http://api.open-notify.org/iss-pass.json?lat=" + latitude + "&lon=" + longitude + "&n=1";
+    URL request = new URL(requestURL);
+    HttpURLConnection connection = (HttpURLConnection) request.openConnection();
+    connection.setRequestMethod("GET");
+    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    StringBuffer response = new StringBuffer();
+    String inputLine;
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
+    }
+
+    return Long.parseLong(
+            OpenNotifyWebService.parseJSON(response.toString()));
+
+
+  }
+}
